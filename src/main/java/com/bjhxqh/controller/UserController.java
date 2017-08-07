98bbc903152838cfc38100def8866802bea08672
magic-5 <309507762@qq.com>
2017-08-07 19:08:10 +0800

    修改后的测试类

--- src/main/java/com/bjhxqh/controller/UserController.java
+++ src/main/java/com/bjhxqh/controller/UserController.java
@@ -3,7 +3,9 @@ package com.bjhxqh.controller;
 import com.bjhxqh.model.po.UserInfo;
 import com.bjhxqh.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,40 +14,38 @@ import java.util.List;
 /**
  * Created by Administrator on 2017-07-17.
  */
-@RestController
+@Controller
 @RequestMapping(value = "/user")
 public class UserController {
     @Autowired
     private UserInfoService userInfoService;
 
     @RequestMapping(value = "/create")
-    @ResponseBody
+    //@ResponseBody
     String create(HttpServletRequest request) {
         UserInfo user = new UserInfo();
         user.setUsername("username123寇鑫");
         user.setPassword("password123寇鑫");
-        userInfoService.insertUser(user);
-        return "插入成功";
-    }
-    @RequestMapping(method = RequestMethod.PUT)
-    @ResponseBody
-    public Object update(HttpServletRequest request) {
-        return "PUT a user";
+        userInfoService.save(user);
+        return "redirect:/user";
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    UserInfo detail(@PathVariable Integer id){
-        UserInfo user = new UserInfo();
-        user.setUsername("张三");
-        user.setPassword("123456");
-        return user;
+    @RequestMapping(value = "view", method = RequestMethod.GET)
+    ModelAndView detail(UserInfo user){
+        ModelAndView result = new ModelAndView("test");
+        if(user.getId()!=null){
+            user = userInfoService.selectByKey(user.getId());
+        }
+        System.out.println(user.getId());
+        System.out.println(user);
+        result.addObject("user",user);
+        return result;
     }
 
     //新增的接口方法
-    @RequestMapping(value = "/list")
+    @RequestMapping(value = {"/list",""})
     @ResponseBody
-    List<UserInfo> list(HttpServletRequest request) {
+    List<UserInfo> list() {
         return userInfoService.selectALL();
     }
 }
