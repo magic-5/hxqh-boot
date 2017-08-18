672ecba2aff4515436d59775200b4a95bc803733
magic-5 <309507762@qq.com>
2017-08-18 10:55:39 +0800

    监理日志

---
+++ src/main/java/com/bjhxqh/mapper/TbSuperviseLogMapper.java
@@ -0,0 +1,7 @@
+package com.bjhxqh.mapper;
+
+import com.bjhxqh.common.base.mapper.MyMapper;
+import com.bjhxqh.model.po.TbSuperviseLog;
+
+public interface TbSuperviseLogMapper extends MyMapper<TbSuperviseLog> {
+}
---
+++ src/main/java/com/bjhxqh/model/po/TbSuperviseLog.java
@@ -0,0 +1,137 @@
+package com.bjhxqh.model.po;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Table(name = "tb_supervise_log")
+public class TbSuperviseLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String projectnum;
+
+    private String projectname;
+
+    private Date logdate;
+
+    private String supervisor;
+
+    private String weather;
+
+    private String constructionnote;
+
+    private String jobcontent;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return projectnum
+     */
+    public String getProjectnum() {
+        return projectnum;
+    }
+
+    /**
+     * @param projectnum
+     */
+    public void setProjectnum(String projectnum) {
+        this.projectnum = projectnum;
+    }
+
+    /**
+     * @return projectname
+     */
+    public String getProjectname() {
+        return projectname;
+    }
+
+    /**
+     * @param projectname
+     */
+    public void setProjectname(String projectname) {
+        this.projectname = projectname;
+    }
+
+    /**
+     * @return logdate
+     */
+    public Date getLogdate() {
+        return logdate;
+    }
+
+    /**
+     * @param logdate
+     */
+    public void setLogdate(Date logdate) {
+        this.logdate = logdate;
+    }
+
+    /**
+     * @return supervisor
+     */
+    public String getSupervisor() {
+        return supervisor;
+    }
+
+    /**
+     * @param supervisor
+     */
+    public void setSupervisor(String supervisor) {
+        this.supervisor = supervisor;
+    }
+
+    /**
+     * @return weather
+     */
+    public String getWeather() {
+        return weather;
+    }
+
+    /**
+     * @param weather
+     */
+    public void setWeather(String weather) {
+        this.weather = weather;
+    }
+
+    /**
+     * @return constructionnote
+     */
+    public String getConstructionnote() {
+        return constructionnote;
+    }
+
+    /**
+     * @param constructionnote
+     */
+    public void setConstructionnote(String constructionnote) {
+        this.constructionnote = constructionnote;
+    }
+
+    /**
+     * @return jobcontent
+     */
+    public String getJobcontent() {
+        return jobcontent;
+    }
+
+    /**
+     * @param jobcontent
+     */
+    public void setJobcontent(String jobcontent) {
+        this.jobcontent = jobcontent;
+    }
+}
