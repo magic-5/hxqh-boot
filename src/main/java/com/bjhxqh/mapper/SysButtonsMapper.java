290431fdf0a746107d1f8fd6c3e02a1c0639cb29
magic-5 <309507762@qq.com>
2017-08-21 17:03:54 +0800

    增加按钮管理后台处理类和对监理工作日志分页查询方法参数修改

---
+++ src/main/java/com/bjhxqh/mapper/SysButtonsMapper.java
@@ -0,0 +1,7 @@
+package com.bjhxqh.mapper;
+
+import com.bjhxqh.common.base.mapper.MyMapper;
+import com.bjhxqh.model.po.SysButtons;
+
+public interface SysButtonsMapper extends MyMapper<SysButtons> {
+}
